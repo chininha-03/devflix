108e4646a39d498226e348c5b58f7f54fe530c1e
chininha-03 <tatazinhoow@gmail.com>
2023-08-18 09:28:52 -0300

    Criado componente MovieCard Adaptações no App.jsx Criado arquivo movieCard.module.css Falta código no movieCard.module.css :)

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,34 +1,39 @@
 import { useEffect } from "react";
+import { useState } from "react";
+
+import "./App.css";
+import MovieCard from "../componentes/movieCard/movieCard";
+
 import logo from "../assets/devflix.png";
 import searchIcon from "../assets/search.svg";
 
-import "./App.css";
-import { useState } from "react";
 
 const App = () => {
-    const [searchTerm,setSearchTerm] = useState ("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const[movies, setMovies] = useState ([])
 
   const apiKey = "e4d577fa"; // https://omdbapi.com/?apikey=e4d577fa&s=Batman
   const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
 
-//   fetch(apiUrl)
-    // .then((response) => response.json())
-    // .then((data) => console.log(data));
+  //   fetch(apiUrl)
+  // .then((response) => response.json())
+  // .then((data) => console.log(data));
 
-    useEffect(() => {
-        searchMovies("Batman")
-    }, []);
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${apiUrl}&s=${title}`);
-        const data = await response.json();
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json();
 
-        console.log(data);
-    };
+    console.log(data);
+    setMovies(data.Search);
+  };
 
-const handleKeyPress = (e) => {
-e.key === "Enter" && searchMovies(searchTerm)
-}
+  const handleKeyPress = (e) => {
+    e.key === "Enter" && searchMovies(searchTerm);
+  };
 
   return (
     <>
@@ -37,10 +42,29 @@ e.key === "Enter" && searchMovies(searchTerm)
           <img src={logo} alt="Logo devflix" />
         </div>
         <div className="search">
-          <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}
-           onKeyDown={handleKeyPress} type="text" placeholder="Pesquise por filmes" />
-          <img src={searchIcon} alt="" onClick={() => searchMovies(searchTerm)} />
+          <input
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyDown={handleKeyPress}
+            type="text"
+            placeholder="Pesquise por filmes"
+          />
+          <img
+            src={searchIcon}
+            alt=""
+            onClick={() => searchMovies(searchTerm)}
+          />
         </div>
+        {movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (<MovieCard key={movie.imdbID} movies={movie} />))}
+          </div>
+        ): (
+          
+          <div className="empty">
+            <h2>Nenhum filme encocntrado 😥</h2>
+          </div>
+          )}
       </div>
     </>
   );
